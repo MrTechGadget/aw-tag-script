463687f56de1f6f94386081509e325cf0060c258
Clark <jclark@mbatest01.southernco.com>
2017-09-01 21:50:31 -0400

    Cleaned up unused code

--- Add-TagToDevices.ps1
+++ Add-TagToDevices.ps1
@@ -2,20 +2,23 @@
 <# Add-TagToDevices Powershell Script Help
 
   .SYNOPSIS
-    This Poweshell script make a REST API call to an AirWatch server.  This particular script is used to add a tag to a list of devices.
+    This Poweshell script adds a selected tag to a list of devices.
     
   .DESCRIPTION
-    To understand the underlying call check:
-    https://<your_AirWatch_Server>/api/mdm/devices/search - this script uses the platform filter to select just iOS devices
-    https://<your_AirWatch_Server>/api/mdm/tags/search - checks for a Supervised tag, and if not found creates one.
-    It is always helpful to validate your parameter using something like the PostMan extension for Chrome
-    https://chrome.google.com/webstore/detail/postman/fhbjgbiflinjbdggehcddcbncdddomop?hl=en
+    This script will take an input of serial numbers from a CSV file, converts them to device IDs. 
+    It queries a list of all Tags in the environment, the user selects the Tag to add the devices to and it adds the Tag in AirWatch for each of those devices.
+
+    This PowerShell script is PowerShell Core compliant and was written with Visual Studio Code on a Mac. It has been tested on Windows and Mac, but should also run on Linux.
+    Setup: 
+    This script takes an input of serial numbers from a CSV file. Sample Included. 
+    It also takes a config file, which houses the API Host, API key and Organization Group ID for your AirWatch environment. 
+    A sample file has been included, just remove the name sample and add your fields, with NO quotations.
 
   .EXAMPLE
-    Add-TagToDevices.ps1 -userName Administrator -password password -tenantAPIKey 4+apikeyw/krandomSstuffIleq4MY6A7WPmo9K9AbM6A= -outputFile c:\Users\Administrator\Desktop\output.txt -endpointURL https://demo.awmdm.com/API/v1/mdm/devices/serialnumber  -inputFile C:\Users\Administrator\Desktop\SerialNumbers1.txt -Verbose
+    Add-TagToDevices.ps1 -Verbose
   
   .PARAMETER userName
-    An AirWatch account in the tenant is being queried.  This user must have the API role at a minimum.
+    An AirWatch account in the tenant is being queried.  This user must have the API role at a minimum. Can be basic or directory user.
 
   .PARAMETER password
     The password that is used by the user specified in the username parameter
@@ -28,38 +31,9 @@
     This will be the https://<your_AirWatch_Server>.  All of the REST endpoints start with a forward slash (/) so do not include that with
     the server name
 
-  .PARAMETER organizationGroupName
-    This will be the organization group name in the AirWatch console.
+  .PARAMETER organizationGroupId
+    This will be the organization group Id in the AirWatch console.
     
-#>
-
-<#
-[CmdletBinding()]
-    Param(
-        [Parameter(Mandatory=$True)]
-        [string]$userName,
-
-        [Parameter(Mandatory=$True)]
-        [string]$password,
-
-        [Parameter(Mandatory=$True)]
-        [string]$tenantAPIKey,
-
-        [Parameter(Mandatory=$True)]
-        [string]$airwatchServer,
-
-        [Parameter(Mandatory=$True)]
-        [string]$organizationGroupName
-)
-
-Write-Verbose "-- Command Line Parameters --"soutehrnco\
-Write-Verbose ("UserName: " + $userName)
-Write-Verbose ("Password: " + $password)
-Write-Verbose ("Tenant API Key: " + $tenantAPIKey)
-Write-Verbose ("AirWatchServer URL: " + $airwatchServer)
-Write-Verbose ("Organization Group Name: " + $organizationGroupName)
-Write-Verbose "-----------------------------"
-Write-Verbose ""
 #>
 
 Function Read-Config {
@@ -70,7 +44,7 @@ Function Read-Config {
     $script:organizationGroupID = $h.groupid
     $script:airwatchServer = $h.host
 }
-
+<# Reads Serial Numbers from Serials.csv file and outputs array of serial numbers. #>
 Function Read-Serials {
     $data = Import-Csv -Path Serials.csv
     $s = @()
@@ -81,10 +55,7 @@ Function Read-Serials {
     return $s
 }
 
-<#
-  This implementation uses Baisc authentication.  See "Client side" at https://en.wikipedia.org/wiki/Basic_access_authentication for a description
-  of this implementation.
-#>
+<#  This implementation uses Basic authentication. #>
 Function Get-BasicUserForAuth {
     $Credential = Get-Credential
     $EncodedUsernamePassword = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($('{0}:{1}' -f $Credential.UserName,$Credential.GetNetworkCredential().Password)))
@@ -111,84 +82,6 @@ Function Build-Headers {
     $header = @{"Authorization" = $authString; "aw-tenant-code" = $tcode; "Accept" = $useJSON; "Content-Type" = $useJSON}
      
     Return $header
-}
-
-Function Get-OrganizationGroupID {
-
-    Param([string]$organizationGroupName, $airwatchServer, [object]$headers)
-
-    Write-Verbose("------------------------------")
-    Write-Verbose("Getting Group ID from Group Name")
-
-    $endpointURL = $airwatchServer + "/api/system/groups/search?groupID=" + $organizationGroupName
-    $webReturn = Invoke-RestMethod -Method Get -Uri $endpointURL -Headers $headers
-    $totalReturned = $webReturn.Total
-    $groupID = -1
-    If ($webReturn.Total = 1) {
-        $groupID = $webReturn.LocationGroups.Id.Value
-        Write-Verbose("Group ID for " + $organizationGroupName + " = " + $groupID)
-    } else {
-        Write-Output("Group Name: " + $organizationGroupName + " not found")
-    }
-    
-    Write-Verbose("------------------------------")
-    Write-Verbose("")
-    Return $groupID
-}
-
-Function Get-SupervidedTagID {
-
-    Param([string]$organizationGroupID, [string]$airwatchServer, [object]$headers)
-
-    Write-Verbose("------------------------------")
-    Write-Verbose("Getting Supervised Tag")
-
-    $endpointURL = $airwatchServer + "/api/mdm/tags/search?organizationgroupid=" + $organizationGroupID
-    $webReturn = Invoke-RestMethod -Method Get -Uri $endpointURL -Headers $headers
-    $supervisedTagExists = $false
-    $supervisedTagID = -1
-    Write-Verbose("Web Return: " + $webReturn.Total)
-    If ([int]$webReturn.Total -gt 0) {
-        foreach($currentTag in $webReturn.Tags) {
-            if ($currentTag.TagName.ToLower() -eq "supervised") {
-                $supervisedTagID = $currentTag.Id.Value
-                $supervisedTagExists = $True
-                Write-Verbose("Found supervised tag: " + $supervisedTagID)
-            }
-        }
-    }
-
-    If ($supervisedTagExists -ne $True) {
-        # This REST API call is not working right now
-        Write-Verbose("Supervised Tag Not Found")
-    }
-
-    Write-Verbose("------------------------------")
-    Write-Verbose("")
-
-    Return $supervisedTagID
-}
-
-<#
-  This function is used to build the JSON to POST to build the Supervised tag if it doesn't exist.  This is not currently working
-  as there is an issue with the Create Tag REST API.
-#>
-Function Get-SupervisedTagJSON {
-
-    Param([int]$organizationGroupID)
-
-	$quoteCharacter = [char]34
-	$tagJSON = "{ " + $quoteCharacter + "TagAvatar" + $quoteCharacter + " : " + $quoteCharacter + $quoteCharacter + ", "
-    $tagJSON = $tagJSON + $quoteCharacter + "TagName" + $quoteCharacter + " : " + $quoteCharacter + "Supervised" + $quoteCharacter + ", "
-    $tagJSON = $tagJSON + $quoteCharacter + "TagType" + $quoteCharacter + " : " + "1" + ", "
-    $tagJSON = $tagJSON + $quoteCharacter + "LocationGroupId" + $quoteCharacter + " : " + $organizationGroupID + ", "
-    $tagJSON = $tagJSON + $quoteCharacter + "id" + $quoteCharacter + " : " + "1" + " }"
-	
-    Write-Verbose "------- JSON to Post---------"
-    Write-Verbose $tagJSON
-    Write-Verbose "-----------------------------"
-    Write-Verbose ""
-	Return $tagJSON
 }
 
 <#  This function builds the JSON to add the tag to all of the devices. #>
@@ -333,14 +226,3 @@ Write-Host("Accepted Items: " + $results.AcceptedItems)
 Write-Host("Failed Items: " + $results.FailedItems)
 Write-Host("------------------------------")
 
-<#
-$endpointURL = $airwatchServer + "/api/mdm/tags/" + $supervisedTagIid + "/adddevices"
-$webReturn = Invoke-RestMethod -Method Post -Uri $endpointURL -Headers $headers -Body $addTagJSON
-
-Write-Verbose("------------------------------")
-Write-Verbose("Results of Add Tags Call")
-Write-Verbose("Total Items: " +$webReturn.TotalItems)
-Write-Verbose("Accepted Items: " + $webReturn.AcceptedItems)
-Write-Verbose("Failed Items: " + $webReturn.FailedItems)
-Write-Verbose("------------------------------")
-#>
