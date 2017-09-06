e97c3dfd454a805e5105a57e856c8c672a2a427e
audioeng <audioeng@gmail.com>
2017-09-06 00:25:15 -0400

    Added new script to export list of devices with a tag

---
+++ Get-ListOfTaggedDevices.ps1
@@ -0,0 +1,170 @@
+<#
+.SYNOPSIS
+  Creates List of Devices that have a particular tag in AirWatch
+.DESCRIPTION
+  This script displays all tags in the Organization group, allowing the user to select a tag. All of the devices with that tag are exported to a CSV file named for that tag.
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  AirWatchConfig.Config
+.OUTPUTS
+  Outputs a CSV file with Devices that have the selected tag.
+.NOTES
+  Version:        1.0
+  Author:         Joshua Clark @audioeng
+  Creation Date:  09/06/2017
+  
+.EXAMPLE
+  Get-ListOfTaggedDevices.ps1
+#>
+
+
+
+Function Read-Config {
+    #from http://tlingenf.spaces.live.com/blog/cns!B1B09F516B5BAEBF!213.entry
+    #
+    Get-Content "AirWatchConfig.Config" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+    $script:tenantAPIKey = $h.awtenantcode
+    $script:organizationGroupID = $h.groupid
+    $script:airwatchServer = $h.host
+}
+
+<#  This implementation uses Basic authentication. #>
+Function Get-BasicUserForAuth {
+    $Credential = Get-Credential
+    $EncodedUsernamePassword = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($('{0}:{1}' -f $Credential.UserName,$Credential.GetNetworkCredential().Password)))
+    
+    Return "Basic " + $EncodedUsernamePassword
+}
+
+Function Build-Headers {
+
+    Param([string]$authoriztionString, [string]$tenantCode, [string]$acceptType, [string]$contentType)
+
+    $authString = $authoriztionString
+    $tcode = $tenantCode
+    $accept = $acceptType
+    $content = $contentType
+
+    Write-Verbose("---------- Headers ----------")
+    Write-Verbose("Authorization: " + $authString)
+    Write-Verbose("aw-tenant-code:" + $tcode)
+    Write-Verbose("Accept: " + $accept)
+    Write-Verbose("Content-Type: " + $content)
+    Write-Verbose("------------------------------")
+    Write-Verbose("")
+    $header = @{"Authorization" = $authString; "aw-tenant-code" = $tcode; "Accept" = $useJSON; "Content-Type" = $useJSON}
+     
+    Return $header
+}
+
+Function Get-Tags {
+    $endpointURL = "https://${airwatchServer}/api/mdm/tags/search?organizationgroupid=${organizationGroupID}"
+    $webReturn = Invoke-RestMethod -Method Get -Uri $endpointURL -Headers $headers
+    $TagArray = New-Object System.Collections.Hashtable
+    foreach ($tag in $webReturn.Tags) {
+        $TagArray.Add($tag.TagName, $tag.Id.Value)
+    }
+    return $TagArray
+}
+
+Function Select-Tag {
+    Param([object]$TagList)
+
+    $selection = $null
+    
+    Do
+    {
+        $mhead
+        Write-Host # empty line
+        $TagArr = @()
+        $i=0
+        foreach($tag in $TagList.keys)
+        {
+            Write-Host -ForegroundColor Cyan "  $($i+1)." $tag
+            $TagArr += $tag
+            $i++
+        }
+        Write-Host # empty line
+        $ans = (Read-Host 'Please enter selection') -as [int]
+    
+    } While ((-not $ans) -or (0 -gt $ans) -or ($TagList.Count -lt $ans))
+    
+    $selection = $ans-1
+    $selectedTag = $TagArr[$selection]
+    return $TagList.$selectedTag
+}
+
+Function Get-Device {
+    Param([string]$SelectedTag)
+
+    $endpointURL = "https://${airwatchServer}/api/mdm/tags/${SelectedTag}/devices?"
+    $webReturn = Invoke-RestMethod -Method Get -Uri $endpointURL -Headers $headers
+    $s = @()
+    foreach ($device in $webReturn.Device) {
+        $s += $device.DeviceId
+        Write-Verbose $device.DeviceId
+    }
+    return $s
+}
+
+<#  This function builds the JSON to add the tag to all of the devices. #>
+Function Set-AddTagJSON {
+
+    Param([Array]$deviceList)
+    
+    Write-Verbose("------------------------------")
+    Write-Verbose("Building JSON to Post")
+    
+    $arrayLength = $deviceList.Count
+    $counter = 0
+    $quoteCharacter = [char]34
+
+    $addTagJSON = "{ " + $quoteCharacter + "BulkValues" + $quoteCharacter + " : { " + $quoteCharacter + "Value" + $quoteCharacter + " : [ "
+    foreach ($currentDeviceID in $deviceList) {
+        $deviceIDString = Out-String -InputObject $currentDeviceID
+        $deviceIDString = $deviceIDString.Trim()
+    
+        $counter = $counter + 1
+        if ($counter -lt $arrayLength) {
+            $addTagJSON = $addTagJSON + $quoteCharacter + $deviceIDString + $quoteCharacter + ", "
+        } else {
+            $addTagJSON = $addTagJSON + $quoteCharacter + $deviceIDString + $quoteCharacter
+        }
+    }
+    $addTagJSON = $addTagJSON + " ] } }"
+    
+    Write-Verbose($addTagJSON)
+    Write-Verbose("------------------------------")
+    Write-Verbose("")
+        
+    Return $addTagJSON
+}
+
+Function Get-DeviceDetails {
+    Param([string]$addTagJSON)
+    
+    $endpointURL = "https://${airwatchServer}/api/mdm/devices/id"
+    $webReturn = Invoke-RestMethod -Method Post -Uri $endpointURL -Headers $headers -Body $addTagJSON
+   
+    return $webReturn.Devices
+}
+
+<#
+Start of Script
+#>
+
+$restUserName = Get-BasicUserForAuth
+Read-Config
+<# Build the headers and send the request to the server. #>
+$useJSON = "application/json"
+$headers = Build-Headers $restUserName $tenantAPIKey $useJSON $useJSON
+
+$TagList = Get-Tags
+$SelectedTag = Select-Tag $TagList
+$TagName = $TagList.keys | Where-Object {$TagList["$_"] -eq [string]$SelectedTag}
+$Devices = Get-Device $SelectedTag
+$DeviceJSON = Set-AddTagJSON $Devices
+$DeviceDetails = Get-DeviceDetails $DeviceJSON
+$DeviceDetails | Export-Csv -Path "${TagName}.csv"
+Write-Host "All Devices with ${TagName} saved to ${TagName}.csv"
